d6f5348b353e837a0237a518abba97a895429577
bhwk <120708160+bhwk@users.noreply.github.com>
2023-03-07 21:55:02 +0800

    Add tailwind lineclamp to dep

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,14 @@
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+			lineClamp: {
+				7: '7',
+				8: '8',
+				9: '9',
+				10: '10'
+			}
+		}
 	},
-	plugins: [require('@tailwindcss/typography')]
+	plugins: [require('@tailwindcss/typography'), require('@tailwindcss/line-clamp')]
 };
