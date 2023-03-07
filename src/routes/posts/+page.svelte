c71f335361d4717dbdb09a69fe0308b4e7eb3af6
bhwk <120708160+bhwk@users.noreply.github.com>
2023-03-07 21:55:42 +0800

    Add post layout, styling post cards

--- src/routes/posts/+page.svelte
+++ src/routes/posts/+page.svelte
@@ -3,12 +3,19 @@
 	import type { PageData } from './$types';
 	export let data: PageData;
 	let { allPosts } = data;
-	console.log(allPosts);
-	console.log(data);
 </script>
 
-{#each allPosts as post}
-	<a href={'posts/' + post.title}>{post.title}<br /></a>
-{/each}
-
-<a href="posts/1">Post one</a>
+<div class="flex flex-1 flex-col gap-12">
+	{#each allPosts as post}
+		<a
+			href={'posts/' + post.title}
+			class="p-4 hover:bg-black transition-colors hover:text-white flex-1 rounded-sm flex flex-col shadow
+			shadow-gray-700"
+		>
+			<div class="font-bold text-2xl pb-2 border-b-2 border-dashed">{post.title}</div>
+			<div class="line-clamp-10 my-4">
+				{post.content}
+			</div>
+		</a>
+	{/each}
+</div>
