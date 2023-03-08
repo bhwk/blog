12911e76e4b2a507b6edec9be0a86e4699622264
bhwk <120708160+bhwk@users.noreply.github.com>
2023-03-08 14:02:05 +0800

    Placeholder screen component, intended for bg

---
+++ src/components/Screen.svelte
@@ -0,0 +1,39 @@
+<div class="screen">
+	<div class="pan-overlay screen-overlay" />
+	<div class="bg-image w-full h-full bg-contain bg-no-repeat" />
+</div>
+
+<style>
+	.screen {
+		width: 500px;
+		border: 3px solid theme('colors.slate.700');
+		aspect-ratio: 10 / 16;
+		border-radius: 1rem;
+		background-color: theme('colors.slate.500');
+		position: relative;
+	}
+
+	.screen-overlay {
+		background: linear-gradient(
+			theme('colors.slate.700'),
+			theme('colors.slate.700') 3px,
+			transparent 3px,
+			transparent 9px
+		);
+		background-size: 100% 9px;
+		width: 100%;
+		height: 100%;
+		animation: pan-overlay 30s infinite linear;
+		position: absolute;
+		opacity: 0.6;
+	}
+
+	@keyframes pan-overlay {
+		from {
+			background-position: 0% 0%;
+		}
+		to {
+			background-position: 0% -100%;
+		}
+	}
+</style>
