216288c83ce3ec5f1eabc117a7759cc6ced0a618
bhwk <120708160+bhwk@users.noreply.github.com>
2023-03-11 01:01:54 +0800

    Styling

--- src/components/Content.svelte
+++ src/components/Content.svelte
@@ -1,3 +1,3 @@
-<div class="prose text-content prose-headings:text-heading prose-h1:text-xl">
+<div class="prose text-content prose-headings:text-heading prose-h1:text-xl prose-a:text-link">
 	<slot />
 </div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,8 +4,8 @@
 </script>
 
 <div class="max-w-4xl mx-auto min-h-screen py-4 px-8">
-	<nav class="flex mb-4 text-link font-bold text-3xl">
-		<div class="flex gap-4">
+	<nav class="flex mb-4 text-link font-semibold text-3xl">
+		<div class="flex gap-6">
 			<a href="/">Home</a>
 			<a href="/posts">Posts</a>
 		</div>
