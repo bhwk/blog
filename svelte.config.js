9ebe4182146791924a548bb377dcecefc6e761b7
bhwk <120708160+bhwk@users.noreply.github.com>
2023-03-11 00:35:54 +0800

    Add support for .md files

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,19 @@
 import adapter from '@sveltejs/adapter-auto';
+import { mdsvex } from 'mdsvex';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-	extensions: ['.svelte'],
+	preprocess: [
+		vitePreprocess(),
+		mdsvex({
+			extensions: ['.md', '.svx'],
+			smartypants: true
+		})
+	],
+	extensions: ['.svelte', '.svx', '.md'],
 
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
